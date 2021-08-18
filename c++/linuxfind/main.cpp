fcd400f2d9f85d28ffaf202c1a0c152f4f8b7b32
Shiyang Wang <shiywang.cs@gmail.com>
2021-08-18 16:26:16 -0400

    update linuxfind

--- c++/linuxfind/main.cpp
+++ c++/linuxfind/main.cpp
@@ -49,9 +49,51 @@ class TypeFilter : public Filter {
 };
 
 class FindCommand {
-
+	void findWithFilters(const File* directory, const vector<Filter*> &filters, vector<File*> &output) {
+	  if(directory->children.empty()) {
+	    return;
+	  }
+	  for(const auto &file : directory->children) {
+	    if(file->isDirectory) {
+		  findWithFilters(file, filters, output);
+	    } else {
+	      bool selectFile = false;
+	      for(const auto &f : filters) {
+	        if(f->apply(file)) {
+	          selectFile = true;
+	          break;
+	        }
+	      }
+	      if(selectFile) {
+	        output.push_back(file);
+	      }
+	    }
+	  }
+	}
+ public:
+  vector<File*> findWithFilters(const File* directory, const vector<Filter*> &filters) {
+	if (!directory->isDirectory) {
+	  cout<<"Exception"<<endl;
+	  return {};
+	}
+	vector<File*> output;
+	findWithFilters(directory, filters, output);
+	return output;
+  }
 };
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  FindCommand fc;
+  File f1("1", 100, 1, false);
+  File f2("2", 1, 2, false);
+  File f3("3", 200, 1, false);
+  File f4("4", 2, 100, false);
+  File f("5", 0, 0, true);
+  f.addChildren({&f1,&f2, &f3, &f4});
+
+  MinSizeFilter filter1(99);
+  TypeFilter filter2(2);
+  for(auto fff : fc.findWithFilters(&f, {&filter1, &filter2})) {
+    cout << fff->name << endl;
+  }
   return 0;
 }
